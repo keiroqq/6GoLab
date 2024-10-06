4ac99374311d1e694b4baacd4214ee737edc8ced
keiroqq <artem.nenashev04@gmail.com>
2024-10-07 00:16:07 +0400

    Update 1.go

--- 1/1.go
+++ 1/1.go
@@ -10,9 +10,10 @@ func factorial(n int) {
 	result := 1
 	for i := 1; i <= n; i++ {
 		result *= i
+		fmt.Printf("Факториал %d = %d\n", i, result)
 		time.Sleep(100 * time.Millisecond)
 	}
-	fmt.Printf("Факториал %d = %d\n", n, result)
+
 }
 
 // Функция для вывода ряда Фибоначчи до номера n
@@ -20,9 +21,9 @@ func fibonacci(n int) {
 	f1, f2 := 0, 1
 	for i := 2; i < n; i++ {
 		f1, f2 = f2, f1+f2
+		fmt.Printf("Число ряда Фибоначчи под номером %d = %d\n", i, f2)
 		time.Sleep(150 * time.Millisecond)
 	}
-	fmt.Printf("Число ряда Фибоначчи под номером %d = %d\n", n, f2)
 }
 
 // Функция для вычисления суммы числового ряда
